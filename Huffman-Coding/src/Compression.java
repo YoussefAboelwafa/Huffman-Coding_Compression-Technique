743728d9e64bf2d7aab1a3fbdc99b69271da0efd
YoussefAboelwafa <youssef.hossam49@gmail.com>
2023-12-19 06:30:32 +0200

    Compression

--- Huffman-Coding/src/Compression.java
+++ Huffman-Coding/src/Compression.java
@@ -1,112 +1,180 @@
-import lombok.Getter;
-
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.util.*;
-
+import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Stack;
 
 public class Compression {
-    @Getter
-    private Map<String, boolean[]> huffman_codes = new HashMap<>();
-    private final Map<String, Integer> frequencies = new HashMap<>();
-    private final PriorityQueue<Node> queue = new PriorityQueue<>();
-    private final int bytes;
+    private int index;
+    private int number_ones = 0;
+    private int size;
     private long original_size = 0;
-    File outputFile;
+    private long compressed_size = 0;
 
-    public Compression(File file, int bytes, int chunk_size) throws IOException {
-        this.bytes = bytes;
-        this.original_size = file.length();
-        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
-            byte[] chunk = new byte[chunk_size];
-            int bytes_read;
-            while ((bytes_read = bis.read(chunk)) != -1) {
-                process_chunk(chunk, bytes_read);
-            }
-        }
-        fill_queue();
-        build_tree();
-        generate_codes(queue.peek(), new boolean[0], 0);
-        encode(file);
-    }
+    public void compress(File file, int n) throws IOException {
 
-    private void process_chunk(byte[] chunk, int bytesRead) {
-        ByteBuffer wrapped = ByteBuffer.wrap(chunk);
-        for (int i = 0; i < bytesRead; i += bytes) {
-            if (bytesRead - i >= bytes) {
-                byte[] byte_arr = new byte[bytes];
-                wrapped.get(byte_arr, 0, bytes);
-                String key = new String(byte_arr);
-                frequencies.put(key, frequencies.getOrDefault(key, 0) + 1);
-            }
-        }
-    }
+        HashMap<Wrapper, Long> frequencies = read_bytes(file, n);
 
-    private void fill_queue() {
-        for (Map.Entry<String, Integer> entry : frequencies.entrySet()) {
-            queue.add(new Leaf(entry.getKey(), entry.getValue()));
-        }
-    }
+        PriorityQueue<Node> queue = new PriorityQueue<>(frequencies.size(), Comparator.comparingLong(Node::getFrequency));
 
-    private void build_tree() {
-        while (queue.size() > 1) {
-            Node left = queue.poll();
-            Node right = queue.poll();
-            Node parent = new Node(left, right);
-            queue.add(parent);
-        }
-    }
+        build_tree(frequencies, queue);
+        HashMap<Wrapper, String> huffman_codes = new HashMap<>();
 
-    private void generate_codes(Node node, boolean[] code, int length) {
-        boolean[] currentCode = Arrays.copyOf(code, length);
-
-        if (node instanceof Leaf leaf) {
-            huffman_codes.put(leaf.getString(), currentCode);
+        Node root = queue.peek();
+        if ((root != null ? root.left : null) == null && (root != null ? root.right : null) == null) {
+            huffman_codes.put(root != null ? root.value : null, "0");
         } else {
-            boolean[] leftCode = Arrays.copyOf(currentCode, length + 1);
-            leftCode[length] = false;
-            generate_codes(node.getLeftNode(), leftCode, length + 1);
+            construct_code_words(huffman_codes, root, "");
+        }
+        compress_file(file, huffman_codes, make_dictionary(root, n), n);
+    }
 
-            boolean[] rightCode = Arrays.copyOf(currentCode, length + 1);
-            rightCode[length] = true;
-            generate_codes(node.getRightNode(), rightCode, length + 1);
+    private HashMap<Wrapper, Long> read_bytes(File file, int n) throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis, 32000);
+        HashMap<Wrapper, Long> frequencies = new HashMap<>();
+        byte[] data = new byte[n];
+        int bytes_read;
+
+        while ((bytes_read = bis.read(data)) != -1) {
+            if (bytes_read < n) {
+                byte[] temp = new byte[bytes_read];
+                System.arraycopy(data, 0, temp, 0, bytes_read);
+                calculate_frequency(temp, frequencies);
+            } else {
+                calculate_frequency(data, frequencies);
+            }
+            data = new byte[n];
+        }
+        bis.close();
+        return frequencies;
+    }
+
+    private void calculate_frequency(byte[] bytes, HashMap<Wrapper, Long> frequencies) {
+        Wrapper temp = new Wrapper(bytes.clone());
+        if (frequencies.containsKey(temp)) frequencies.put(temp, frequencies.get(temp) + 1);
+        else frequencies.put(temp, (long) 1);
+    }
+
+    private void build_tree(HashMap<Wrapper, Long> frequencies, PriorityQueue<Node> queue) {
+        for (Wrapper key : frequencies.keySet()) {
+            queue.add(new Node(key, frequencies.get(key)));
+        }
+        construct_tree(queue);
+    }
+
+    private void construct_tree(PriorityQueue<Node> queue) {
+        int i = queue.size();
+        while (i > 1) {
+            Node first = queue.poll();
+            Node second = queue.poll();
+            assert first != null;
+            assert second != null;
+            long frequencies_sum = first.getFrequency() + second.getFrequency();
+            first.setCode("0");
+            second.setCode("1");
+            Node current = new Node(frequencies_sum, first, second);
+            queue.add(current);
+            i--;
         }
     }
 
+    private void construct_code_words(HashMap<Wrapper, String> huffman_codes, Node root, String s) {
+        s = s + root.getCode();
+        if (root.left == null && root.right == null) {
+            huffman_codes.put(root.value, s);
+            root.setCode(s);
+        } else {
+            assert root.left != null;
+            construct_code_words(huffman_codes, root.left, s);
+            construct_code_words(huffman_codes, root.right, s);
+        }
+    }
 
-    private void encode(File inputFile) throws IOException {
-        String outputFileName = "20012263." + bytes + "." + inputFile.getName() + ".hc";
-        outputFile = new File(inputFile.getParent(), outputFileName);
+    private void compress_file(File file, HashMap<Wrapper, String> huffman_codes, byte[] dictionary, int n) throws IOException {
+        this.original_size = file.length();
+        String old_file = file.getName();
+        String compressed_file = file.getAbsolutePath().replace(old_file, "20012263." + n + "." + old_file + ".hc");
+        FileOutputStream fos = new FileOutputStream(compressed_file);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        bos.write(dictionary);
+        write_file(file, bos, n, huffman_codes);
+        this.compressed_size = new File(compressed_file).length();
+    }
 
-        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(inputFile));
-             FileOutputStream fos = new FileOutputStream(outputFile);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+    public long getCompressed_size() {
+        return this.compressed_size;
+    }
 
-            oos.writeObject(huffman_codes);
-            byte[] chunk = new byte[bytes];
-            int bytesRead;
-            while ((bytesRead = bis.read(chunk)) != -1) {
-                if (bytesRead == bytes) {
-                    String key = new String(chunk);
-                    boolean[] huffmanCode = huffman_codes.get(key);
-                    if (huffmanCode != null) {
-                        byte[] encodedChunk = new byte[(huffmanCode.length + 7) / 8];
-                        fos.write(encodedChunk);
-                    } else {
-                        throw new IOException("No Huffman code for input chunk: " + key);
-                    }
+    public double getCompression_ratio() {
+        return ((double) this.compressed_size / this.original_size) * 100;
+    }
+
+    private byte[] make_dictionary(Node root, int n) {
+        Stack<Node> stk = new Stack<>();
+        stk.push(root);
+        StringBuilder dictionary = new StringBuilder();
+
+        while (!stk.isEmpty()) {
+            Node temp = stk.pop();
+            if (temp.left == null && temp.right == null) {
+                dictionary.append("1");
+                number_ones++;
+                if (temp.value.data.length < n) {
+                    this.index = number_ones;
+                    this.size = temp.value.data.length;
                 }
+                dictionary.append(new String(temp.value.data, StandardCharsets.ISO_8859_1));
+            } else dictionary.append("0");
+            if (temp.right == null && temp.left != null) stk.push(temp.left);
+            else if (temp.left == null && temp.right != null) stk.push(temp.right);
+            else if (temp.left != null) {
+                stk.push(temp.right);
+                stk.push(temp.left);
             }
         }
+        StringBuilder sb = new StringBuilder();
+        sb.append(n).append(",").append(this.index).append(",").append(this.size);
+        sb.append(",").append(sb.length()).append(",").append(sb);
+        return sb.toString().getBytes(StandardCharsets.ISO_8859_1);
     }
 
+    private void write_file(File file, BufferedOutputStream bos, int n, HashMap<Wrapper, String> huffman_codes) throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+        BufferedInputStream bis = new BufferedInputStream(fis, 32000);
+        byte[] data = new byte[n];
+        int bytes_read;
+        StringBuilder sb = new StringBuilder();
+        Wrapper wrapper;
+        while ((bytes_read = bis.read(data)) != -1) {
+            if (bytes_read < n) {
+                byte[] temp = new byte[bytes_read];
+                System.arraycopy(data, 0, temp, 0, bytes_read);
+                wrapper = new Wrapper(temp.clone());
+            } else {
+                wrapper = new Wrapper(data.clone());
+            }
+            sb.append(huffman_codes.get(wrapper));
+            while (sb.length() >= 8) {
+                String written_string = sb.substring(0, 8);
+                bos.write(((Integer) Integer.parseInt(written_string, 2)).byteValue());
+                sb.delete(0, 8);
+            }
+            data = new byte[n];
+        }
 
-    public long get_compressed_size() {
-        return outputFile.length();
-    }
-
-    public double get_compression_ratio() {
-        return (double) get_compressed_size() / original_size;
+        int count_zeros = 0;
+        if (!sb.isEmpty()) {
+            for (int i = sb.length(); i < 8; i++) {
+                sb.append("0");
+                count_zeros++;
+            }
+            bos.write(((Integer) Integer.parseInt(sb.toString(), 2)).byteValue());
+        }
+        bos.write(((byte) count_zeros));
+        bos.close();
+        bis.close();
     }
 
 }
